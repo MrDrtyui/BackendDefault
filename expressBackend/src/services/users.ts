57dd6540011cbd827489213e72627545dc17c743
Drtyui <drtyuicode@gmail.com>
2025-01-14 12:56:50 +0500

    NginxAdd

--- expressBackend/src/services/users.ts
+++ expressBackend/src/services/users.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export const getAllUsers = async () => {
     try{
-        return prisma.user.findMany({include: {posts: true}});
+        return await prisma.user.findMany({include: {posts: true}});
     } catch (error) {
         console.log(error);
         throw new Error("Failed to create user");
@@ -13,7 +13,7 @@ export const getAllUsers = async () => {
 
 export const createUser = async ( name:string, password:string ) => {
     try{
-        return prisma.user.create({
+        return await prisma.user.create({
             data: {
                 name,
                 password
